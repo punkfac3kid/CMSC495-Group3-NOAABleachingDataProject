316cf9031ae857e71ba8cef7ae0c61e09b059b65
Collin Hunter <punkfac3kid@gmail.com>
2021-11-06 15:25:55 -0400

    Geoplotting test branch (#2)
    * First Plotting Test
    
    This is a push to get my files uploaded on the project. This is non-functional as of now, but doesn't error out. I am still working on the plotting.
    
    * Changed some lines for consistent testing of the plotter.

---
+++ GeoPlotTest/GeoPlot.py
@@ -0,0 +1,31 @@
+'''This file is going to be used as a sample test
+ bed for the functionality needed to plot coordinates
+ on a map for the Coral Bleaching application. Everything
+ is subject to change.'''
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import geopandas as gpd
+from shapely import geometry
+from shapely.geometry import Point, Polygon
+
+coral_map = gpd.read_file('.\shapemap\Coral_and_Hard_Bottom_Habitats_in_Florida.shp')
+
+fig,ax = plt.subplots(figsize = (10,10))
+coral_map.plot(ax = ax)
+
+
+df = pd.read_csv('test_DF.csv')
+crs = 'epsg:4326'
+df.head()
+
+geometry = [Point(xy) for xy in zip (df["Long"], df["Lat"])]
+
+
+geo_df = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
+geo_df.head()
+
+fig.ax = plt.subplots(figsize = (10, 10))
+coral_map.plot(ax=ax, alpha=.4, color="cyan")
+
+geo_df.plot(ax=ax, markersize=20, color="purple", marker="^", label="Bleach Report")
