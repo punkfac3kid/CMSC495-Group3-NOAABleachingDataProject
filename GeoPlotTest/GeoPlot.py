e3fa7beed0d5fa4b6cd79a68a68dfacb37c12c42
Collin Hunter <punkfac3kid@gmail.com>
2021-11-09 15:20:24 -0500

    Updated GeoPlot to do map comparisons but still working on cleaning CSVs as only 2014 works

--- GeoPlotTest/GeoPlot.py
+++ GeoPlotTest/GeoPlot.py
@@ -6,13 +6,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import geopandas as gpd
+from shapely import geometry
 from shapely.geometry import Point
 
 coral_map = gpd.read_file('shapeMap\Florida Shore + Reef Map.shp')
-df = pd.read_csv('BW_2014_Florida.csv', encoding='ISO-8859-1')
+
+df = pd.read_csv('bleach_watch_2014.csv', encoding='ISO-8859-1')
+df2 = pd.read_csv('bleach_watch_2018.csv', encoding='Windows-1252')
+
+df = df[df['GIS Longitude'].notna()]
+df2 = df2[df2['GIS Longitude'].notna()]
 
 
-def generate_bleach_map(year_df, coral_map):
+def generate_bleach_map(year_df, coral_map, year_title):
 
     geometry = [Point(xy)
                 for xy in zip(year_df['GIS Longitude'], year_df['GIS Latitude'])]
@@ -26,9 +32,44 @@ def generate_bleach_map(year_df, coral_map):
     geo_df[geo_df['Bleaching?'] == 'NO'].plot(
         ax=ax, markersize=20, color='red', marker='x', label='No')
 
-    plt.legend(prop={'size' : 10})
-    fig.suptitle('Bleaching reports from selected year', fontsize=16)
+    plt.legend(prop={'size': 10})
+    fig.suptitle('Bleaching reports from ' + year_title, fontsize=16)
     plt.show()
 
 
-generate_bleach_map(df, coral_map)
+def generate_comparison_map(year_one_df, year_two_df, coral_map, year_title1, year_title2):
+
+    geometry_yearone = [Point(xy)
+                        for xy in zip(pd.to_numeric(year_one_df['GIS Longitude']), pd.to_numeric(year_one_df['GIS Latitude']))]
+
+    geo_df_yearone = gpd.GeoDataFrame(
+        year_one_df, crs='epsg:4326', geometry=geometry_yearone)
+
+    geometry_yeartwo = [Point(xy)
+                        for xy in zip(year_two_df['GIS Longitude'], year_two_df['GIS Latitude'])]
+
+    geo_df_yeartwo = gpd.GeoDataFrame(
+        year_two_df, crs='epsg:4326', geometry=geometry_yeartwo)
+
+    fig, (ax1, ax2) = plt.subplots(
+        ncols=2, sharex=True, sharey=True, figsize=(15, 10))
+
+    coral_map.plot(ax=ax1, alpha=.4, color="black")
+    coral_map.plot(ax=ax2, alpha=.4, color="grey")
+
+    geo_df_yearone[geo_df_yearone['Bleaching?'] == 'YES'].plot(
+        ax=ax1, markersize=20, color='green', marker='o', label='Yes')
+    geo_df_yearone[geo_df_yearone['Bleaching?'] == 'NO'].plot(
+        ax=ax1, markersize=20, color='red', marker='x', label='No')
+
+    geo_df_yeartwo[geo_df_yeartwo['Bleaching?'] == 'YES'].plot(
+        ax=ax2, markersize=20, color='green', marker='o', label='Yes')
+    geo_df_yeartwo[geo_df_yeartwo['Bleaching?'] == 'NO'].plot(
+        ax=ax2, markersize=20, color='red', marker='x', label='No')
+
+    plt.legend(prop={'size': 10})
+    fig.suptitle('Bleaching reports from left to right for years ' +
+                 year_title1 + ' and ' + year_title2, fontsize=16)
+    plt.show()
+
+generate_bleach_map(df, coral_map, '2018')
