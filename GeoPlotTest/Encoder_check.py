5336020b73a39c77be0ea2dd234ac1ecf760db7d
Collin Hunter <punkfac3kid@gmail.com>
2021-12-07 10:48:57 -0500

    Deleting old directories and pulling in new geoplot

--- GeoPlotTest/Encoder_check.py
+++
@@ -1,9 +0,0 @@
-import chardet as chdet
-
-file_name = input('Input file name to check:')
-raw_file = open(file_name, 'rb').read()
-result = chdet.detect(raw_file)
-
-char_enc = result['encoding']
-
-print(char_enc)
