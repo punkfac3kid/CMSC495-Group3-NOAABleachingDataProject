9aba7177e1ba2f33a37e55ee260f0a41491cd11f
Collin Hunter <punkfac3kid@gmail.com>
2021-12-08 21:43:35 -0500

    Changed marker sizes back to 5

--- geoplot.py
+++ geoplot.py
@@ -7,10 +7,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import geopandas as gpd
 from shapely.geometry import Point
-import warnings
-
-
-warnings.filterwarnings(action='ignore', message='UserWarning:')
 
 YEAR_COLOR_LIST = ['red', 'green', 'blue',
                    'yellow', 'cyan', 'orange', 'purple']
@@ -42,43 +38,43 @@ def generate_bleach_map(df):
 
     if not geo_df_2014.empty:
         geo_df_2014.plot(
-            ax=ax, markersize=2, color=YEAR_COLOR_LIST[0], marker='o', label='2014')
+            ax=ax, markersize=5, color=YEAR_COLOR_LIST[0], marker='o', label='2014')
 
     geo_df_2015 = geo_df[geo_df['YEAR'].isin(['2015', 2015, float(2015)])]
 
     if not geo_df_2015.empty:
         geo_df[geo_df['YEAR'].astype(str) == '2015'].plot(
-            ax=ax, markersize=2, color=YEAR_COLOR_LIST[1], marker='x', label='2015')
+            ax=ax, markersize=5, color=YEAR_COLOR_LIST[1], marker='x', label='2015')
 
     geo_df_2016 = geo_df[geo_df['YEAR'].isin(['2016', 2016, float(2016)])]
 
     if not geo_df_2016.empty:
         geo_df[geo_df['YEAR'].astype(str) == '2016'].plot(
-            ax=ax, markersize=2, color=YEAR_COLOR_LIST[2], marker='o', label='2016')
+            ax=ax, markersize=5, color=YEAR_COLOR_LIST[2], marker='o', label='2016')
 
     geo_df_2017 = geo_df[geo_df['YEAR'].isin(['2017', 2017, float(2017)])]
 
     if not geo_df_2017.empty:
         geo_df[geo_df['YEAR'].astype(str) == '2017'].plot(
-            ax=ax, markersize=2, color=YEAR_COLOR_LIST[3], marker='x', label='2017')
+            ax=ax, markersize=5, color=YEAR_COLOR_LIST[3], marker='x', label='2017')
 
     geo_df_2018 = geo_df[geo_df['YEAR'].isin(['2018', 2018, float(2018)])]
 
     if not geo_df_2018.empty:
         geo_df[geo_df['YEAR'].astype(str) == '2018'].plot(
-            ax=ax, markersize=2, color=YEAR_COLOR_LIST[4], marker='o', label='2018')
+            ax=ax, markersize=5, color=YEAR_COLOR_LIST[4], marker='o', label='2018')
 
     geo_df_2019 = geo_df[geo_df['YEAR'].isin(['2019', 2019, float(2019)])]
 
     if not geo_df_2019.empty:
         geo_df[geo_df['YEAR'].astype(str) == '2019'].plot(
-            ax=ax, markersize=2, color=YEAR_COLOR_LIST[5], marker='x', label='2019')
+            ax=ax, markersize=5, color=YEAR_COLOR_LIST[5], marker='x', label='2019')
 
     geo_df_2020 = geo_df[geo_df['YEAR'].isin(['2020', 2020, float(2020)])]
 
     if not geo_df_2020.empty:
         geo_df[geo_df['YEAR'].astype(str) == '2020'].plot(
-                ax=ax, markersize=2, color=YEAR_COLOR_LIST[6], marker='o', label='2020')
+                ax=ax, markersize=5, color=YEAR_COLOR_LIST[6], marker='o', label='2020')
 
     del geo_df_2014, geo_df_2015, geo_df_2016, geo_df_2017, geo_df_2018, geo_df_2019, geo_df_2020
 
